6cd3b438699e42ef93f29343310424b061d7c356
Nutchanon Chantrasup <beam.nutchanon@bitkub.com>
2025-07-08 02:16:42 +0700

    fix: wrong token api instruction (blockscout api)

--- src/resources/base-knowledge/blockscout.ts
+++ src/resources/base-knowledge/blockscout.ts
@@ -55,7 +55,7 @@ GET /addresses/{address_hash}/transactions
 List transactions for an address. Filter with query param: filter.
 GET /addresses/{address_hash}/token-transfers
 List token transfers for an address. Filter with type, filter, token.
-GET /addresses/0x5062CB0af889eB77e59482a0D71448Fb3061d3bf/tokens
+GET /addresses/{address_hash}/tokens
 List tokens held by an address. Filter with optional query param: type ERC20 ERC721 ERC1155.
 GET /addresses/{address_hash}/internal-transactions
 List internal transactions for an address. Filter with query param: filter.
